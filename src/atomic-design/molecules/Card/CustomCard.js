a5dc96fa48a31c9fa4747b88e4262b2d3ba98b07
micTrb <aedonem@gmail.com>
2022-10-18 18:37:38 +0200

    opacity card added

--- src/atomic-design/molecules/Card/CustomCard.js
+++ src/atomic-design/molecules/Card/CustomCard.js
@@ -36,9 +36,9 @@ const CustomCard = (props) => {
 
     <div onClick={handleGetCharacterDetails} className="cursor-pointer flex justify-center">
       <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-lg w-full ">
-        <img className="w-full h-96 md:h-auto object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
+        <img className=" w-full h-96 md:h-auto object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
           src={image} alt="" />
-        <div className="p-6 w-full max-w-xl flex flex-col justify-start bg-slate-700 rounded-b-lg md:rounded-none md:rounded-r-lg">
+        <div className="hover:bg-slate-500 p-6 w-full max-w-xl flex flex-col justify-start bg-slate-700 rounded-b-lg md:rounded-none md:rounded-r-lg">
           <h5 className="pb-4 text-white text-start text-4xl font-bold mb-2">{name}</h5>
           <p className="text-white text-start text-xl py-2">
             <span className="font-bold">Status: </span>{status}
