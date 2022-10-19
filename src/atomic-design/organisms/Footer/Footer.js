41cbccc50dc6fca2ff66e67521e16751771895d5
micTrb <aedonem@gmail.com>
2022-10-19 17:11:30 +0200

    footer changed

--- src/atomic-design/organisms/Footer/Footer.js
+++ src/atomic-design/organisms/Footer/Footer.js
@@ -9,11 +9,13 @@ const Footer = () => {
     return (
         <div class="mt-48 w-full p-8 flex items-center justify-center bg-slate-700">
             <div class="md:w-2/3 w-full px-4 text-white flex flex-col">
-                <div class="flex flex-col">
-                    <a target="_blank" href="https://www.michaeltarabella.com">
-                        <p class="w-full text-center my-12 text-white">Copyright © 2022 Michael Tarabella</p>
-                    </a>
-                </div>
+
+                <p class="w-full text-center text-white">
+                    <a target="_blank" href="https://www.michaeltarabella.com">Developed by Michael Tarabella  </a>
+                    <a target="_blank" href="https://www.michaeltarabella.com">| Powered by Tailwind</a>
+                </p>
+
+
             </div>
         </div>
     )
